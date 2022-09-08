36dbb93a296f5031fea968e90a0bbfdb2375351b
Blwert <levell.curtis>
2022-09-08 17:04:22 -0400

    HomePage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FlexLayoutModule } from "@angular/flex-layout"
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { CheckoutPageComponent } from './component/checkout-page/checkout-page.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"",
+  component: HomePageComponent
+
+},
+{
+  path:"home",
+  component: HomePageComponent
+},
+{
+  path:"checkout",
+  component: CheckoutPageComponent
+}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule,
+           FlexLayoutModule,
+           FormsModule,
+           HttpClientModule
+          ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { CheckoutPageComponent } from './component/checkout-page/checkout-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NavBarComponent,
+    CheckoutPageComponent
+   
   ],
   imports: [
     BrowserModule,
