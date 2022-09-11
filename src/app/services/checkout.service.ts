a552c180443630f3673954b340c931ae6e53fddf
kirwi <kyleairwin@gmail.com>
2022-09-10 18:00:54 -0700

    working checkout service and bookshelf icon

--- src/app/services/checkout.service.ts
+++ src/app/services/checkout.service.ts
@@ -1,23 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Book } from 'src/app/models/book';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckoutService {
 
-  //books:Book[] = [];
+  books:Book[] = [];
 
-  url:string = 'https://gutendex.com/books/'
+  addToCart(book: Book): void {
+    if (!this.books.some((item: Book) => item.id === book.id))
+    this.books.push(book);
+  }
 
-  constructor(private httpClient:HttpClient) { }
+  removeFromCart(book: Book): void {
+    this.books = this.books.filter((item: Book) => item.id !== book.id);
+  }
 
- /* getBooks():Observable<Book[]>{
-    return this.httpClient.get(this.url,{
-      headers:{
-      accept:"application/json"
-    }
-  }) as Observable<Book[]>;
-} */
+  getItems(): Book[] {
+    return this.books;
+  }
+
+  clearCart(): void {
+    this.books = [];
+  }
 }
