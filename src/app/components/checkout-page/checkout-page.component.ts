a5d6f17359e31b64fae3682d02d19a1ea69f237a
rashad279 <rickr12@mail.broward.edu>
2022-09-11 15:30:11 -0400

    designed and created methods for the bookshelf/cart

--- src/app/components/checkout-page/checkout-page.component.ts
+++ src/app/components/checkout-page/checkout-page.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Book } from 'src/app/models/book';
 import { CheckoutService } from 'src/app/services/checkout.service';
+import { Result } from 'src/app/models/result';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-checkout-page',
@@ -8,28 +11,35 @@ import { CheckoutService } from 'src/app/services/checkout.service';
 })
 export class CheckoutPageComponent implements OnInit {
 
- // books:Book[] = [];
-
- visibility:boolean = false;
+  books:Book[] = [];
+  visibility:boolean = false;
 
   constructor(private checkoutService:CheckoutService) { }
 
   ngOnInit(): void {
-    /* To get the Books info from the API
+    this.getBookshelf();
+  }
 
+  getBookshelf(){
     this.checkoutService.getBooks().subscribe({
       next:(data:Book[])=>{
         this.books=data;
       },
       error:()=>{
-        console.log("Failed to load book");
+        console.log("unable to retrieve Todos");
       }
-    }); */
+    });
+  }
+
+  removeBook(book:Book){
+    this.checkoutService.removeFromCart(book);
+    this.getBookshelf();
   }
 
   switchVisible():void {
     this.visibility = !this.visibility;
   }
+
 
 
 }
