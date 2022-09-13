ec348d8d4cb8c2d1b3e964e3ec31a7337b0af1ec
Carlos Home PC <CarlosMaciasCusiter@gmail.com>
2022-09-12 20:13:21 -0700

    adding register-service functionality

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,21 +1,25 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Book } from '../models/book';
-import { User } from '../models/user';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { observable, Observable } from "rxjs";
+import { Book } from "../models/book";
+import { User } from "../models/user";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class RegisterService {
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
-  constructor(private httpClient:  HttpClient) { }
+  urlDatabase: string = "http://18.218.155.146:8083/data/register";
 
-  urlDatabase:string = "http://18.218.155.146:8083/data/register";
+  // response = this.httpClient.post<String>(this.urlDatabase, user);
 
-  doPost(user:User): Observable<User> {
-    let body: User = user;
-    return this.httpClient.post<User>(this.urlDatabase, body);
+  doPost(user: User): void {
+    let response = this.httpClient.post<any>(this.urlDatabase, user);
+    response.subscribe((x) => {
+      console.log(x);
+    });
+    this.router.navigate(["/"]);
   }
-
 }
